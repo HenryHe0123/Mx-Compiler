6f97650dc3e09791b9bbbcac32de20d9cf806716
HenryHe0123 <13600668331@163.com>
2023-08-23 20:57:52 +0800

    update LocalJudge for Assembly

--- src/LocalJudge.java
+++ src/LocalJudge.java
@@ -8,12 +8,16 @@ public class LocalJudge {
     /* INSTRUCTION:
      * To use this local judge, you need to enable assertions for your IDE.
      * For IntelliJ IDEA, go to Run -> Edit Configurations -> VM options, and add "-ea" to the text box.
-     * You may also need to install libc6-dev-i386 ("sudo apt-get install libc6-dev-i386") for wsl to run clang-15 -m32.
      * While judging, the program will generate some temporary files under tmpFolder, which will be auto-cleared after test.
      * Please make sure that tmpFolder is useless before judging, preventing from deleting your files unexpectedly.
+     * Be free to turn off any tests you want to skip and change the configuration below.
+     *
+     * REQUIREMENTS:
+     * You may need to install libc6-dev-i386 ("sudo apt-get install libc6-dev-i386") to run clang-15 -m32 for IR test
+     * and install ravel (a RISC-V simulator) to run riscv32 assembly for Assembly test.
      *
      * REMARKS:
-     * In some cases, codegen/t20.mx may fail for unknown reasons. (maybe related to the mismatch of target platform)
+     * In some cases, codegen/t20.mx may fail IR test for unknown reasons (maybe related to the mismatch of target platform).
      * You can use ravel to test this testcase separately, or try using phi instead of alloca for logic expression.
      */
     public static final String RESET = "\u001B[0m";
@@ -29,11 +33,121 @@ public class LocalJudge {
     private static final String clang = "clang-15";
     private static final boolean showSemanticDetail = false;
     private static final boolean showIRDetail = true;
+    private static final boolean showAsmDetail = true;
 
     public static void main(String[] args) throws Exception {
-        testSemantic(true);
-        testIR(true);
+        testSemantic(false);
+        testIR(false);
+        testAssembly(true);
     }
+
+    public static void testAssembly(boolean on) {
+        if (!on) {
+            System.out.println(RESET + "Skip assembly test.");
+            return;
+        }
+        System.out.println(RESET + "Start testing assembly...");
+        initTmpFolder();
+        String fileName;
+        var failList = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(codeGenFolderPathPrefix + "judgelist.txt"))) {
+            while ((fileName = reader.readLine()) != null) {
+                //fileName = fileName.substring(2);
+                if (showAsmDetail) System.out.println(YELLOW + fileName + ": ");
+                if (checkAssembly(fileName)) {
+                    if (showAsmDetail) System.out.println(GREEN + "Pass!");
+                } else {
+                    failList.add(fileName);
+                    if (showAsmDetail) System.out.println(RED + "Fail!");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        if (failList.isEmpty()) {
+            System.out.println(GREEN + "All assembly test passed!");
+        } else {
+            System.out.println(RED + "Some assembly test failed:");
+            failList.forEach(filename -> System.out.println(RED + filename));
+        }
+        cleanUp();
+    }
+
+    private static boolean checkAssembly(String fileName) {
+        try {
+            //compile to assembly
+            final String mxFileName = codeGenFolderPathPrefix + fileName;
+            final String asmFileName = tmpFilePath + ".s";
+            InputStream compilerInput = new FileInputStream(mxFileName);
+            try (PrintStream compilerOutput = new PrintStream(new FileOutputStream(asmFileName))) {
+                Compiler.compile(compilerInput, null, compilerOutput);
+            } catch (Error er) {
+                System.err.println(er.getText());
+                System.err.println("compile error");
+                return false;
+            }
+
+            //get input and standard output & exitCode
+            final String input = getContentFromFile(mxFileName, "=== input ===");
+            final String output = getContentFromFile(mxFileName, "=== output ===");
+            int exitCode = getExitCodeFromFile(mxFileName, "ExitCode:");
+
+            //write input to tmp.in
+            final String inFileName = tmpFilePath + ".in";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(inFileName))) {
+                writer.write(input);
+            }
+
+            //create tmp.out & tmp-ravel.out
+            final String ravelOutFileName = tmpFilePath + "-ravel.out";
+            final String outFileName = tmpFilePath + ".out";
+            File file = new File(outFileName);
+            assert file.exists() || file.createNewFile() : "create tmp.out failed";
+
+            //redirect process output to tmp-ravel.out
+            ProcessBuilder processBuilder = new ProcessBuilder("wsl", "/usr/local/opt/bin/ravel",
+                    "--input-file=" + inFileName, "--output-file=" + outFileName,
+                    asmFileName, builtinPath);
+
+            processBuilder.redirectOutput(new File(ravelOutFileName));
+            processBuilder.redirectErrorStream(true);
+
+            //run ravel
+            Process process = processBuilder.start();
+            int processExitCode = process.waitFor();
+            if (processExitCode != 0) {
+                if (showAsmDetail) {
+                    String ravelOutput = Files.readString(Paths.get(ravelOutFileName));
+                    System.err.println("ravel error");
+                    System.err.println(ravelOutput);
+                }
+                return false;
+            }
+
+            //check exitCode
+            int ravelExitCode = getExitCodeFromFile(ravelOutFileName, "exit code:");
+            if (ravelExitCode != exitCode) {
+                if (showAsmDetail) System.err.println("exit code not match");
+                return false;
+            }
+
+            //check output
+            String processOutput = Files.readString(Paths.get(outFileName)).trim();
+            if (!processOutput.equals(output)) {
+                if (showAsmDetail) System.err.println("output not match");
+                return false;
+            }
+
+            return true;
+        } catch (Exception e) {
+            System.err.println("other error occurred");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    //----------------------------------- IR ----------------------------------------
 
     public static void testIR(boolean on) {
         if (!on) {
@@ -131,7 +245,7 @@ public class LocalJudge {
             //get input and standard output & exitCode
             final String input = getContentFromFile(mxFileName, "=== input ===");
             final String output = getContentFromFile(mxFileName, "=== output ===");
-            int exitCode = getExitCodeFromFile(mxFileName);
+            int exitCode = getExitCodeFromFile(mxFileName, "ExitCode:");
 
             //write input to tmp.in
             final String inFileName = tmpFilePath + ".in";
@@ -163,8 +277,9 @@ public class LocalJudge {
             }
 
             return true;
-        } catch (Exception err) {
-            System.err.println("other error occurred: probably due to incorrect file path");
+        } catch (Exception e) {
+            System.err.println("other error occurred");
+            e.printStackTrace();
             return false;
         }
     }
@@ -188,12 +303,12 @@ public class LocalJudge {
         return content.toString().trim();
     }
 
-    private static int getExitCodeFromFile(String fileName) {
+    private static int getExitCodeFromFile(String fileName, String marker) {
         int exitCode = -1;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                if (line.startsWith("ExitCode:")) {
+                if (line.startsWith(marker)) {
                     exitCode = Integer.parseInt(line.substring(line.indexOf(":") + 1).trim());
                     break;
                 }
